2d6f1aae111f8b91b98633ba10f37db7cdfc2526
shaaaaaaq <42686442+shaaaaaaq@users.noreply.github.com>
2018-08-25 04:48:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,4 +27,23 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+client.on('message', message => {
+    if (message.content === 'help') {
+        let helpEmbed = new Discord.RichEmbed()
+        .setTitle('**أوامر الميوزك...**')
+        .setDescription('**برفكس البوت (!)**')
+        .addField('play', 'لتشغيل اغنية')
+        .addField('join', 'دخول رومك الصوتي')
+        .addField('disconnect', 'الخروج من رومك الصوتي')
+        .addField('skip', 'تخطي الأغنية')
+        .addField('pause', 'ايقاف الاغنية مؤقتا')
+        .addField('resume', 'تكملة الاغنية')
+        .addField('queue', 'اظهار قائمة التشغيل')
+        .addField('np', 'اظهار الاغنية اللي انت مشغلها حاليا')
+        .setFooter('(general_commands) لاظهار الاوامر العامة')
+      message.channel.send(helpEmbed);
+
+
+
+
 client.login("NDgwNDcyMzc1MDQ1OTE0NjM0.DloXYQ.FI9Bfxgrt0jmS2sog5LCRgstj4Y");
