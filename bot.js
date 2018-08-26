4697a0866fbc3e24e5835e16931a505966a334a4
shaaaaaaq <42686442+shaaaaaaq@users.noreply.github.com>
2018-08-26 07:54:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -341,5 +341,27 @@ client.on('message', message => {
       message.channel.send(helpEmbed);
     }
 });
+client.on('message', message => {
+  if (!message.content.startsWith(PREFIX)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "439187325503930369") return;
+
+if (message.content.startsWith(PREFIX + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
+	 console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+}
+
+if (message.content.startsWith(PREFIX + 'setname')) {
+  client.user.setUsername(argresult).then
+	  message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+}
+if (message.content.startsWith(PREFIX + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
+});
 
 client.login(process.env.BOT_TOKEN);
