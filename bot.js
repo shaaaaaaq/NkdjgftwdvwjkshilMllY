f6eb2605e4bf4cb546b5b2480311fbd7b1466e6e
shaaaaaaq <42686442+shaaaaaaq@users.noreply.github.com>
2018-08-26 08:11:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -385,6 +385,23 @@ let args = message.content.split(' ').slice(1).join(' ');
         
 }
 });
-
+client.on('ready', function(){    
+    var ms = 150000 ;    // السرعة لا  تغيرها عشان ما تتبند 
+    var prefix = "Sh"; 
+    var setGame = [`المستخدمين : ${client.users.size}`,`${prefix}help`,`االسيرفرات : ${client.guilds.size}`];    // لا تلعب بشيء
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://www.youtube.com/AlphaCodes!`);    // حقوقنا فضلا اتركها وشائنها
+}, ms);    
+    
+});
 
 client.login(process.env.BOT_TOKEN);
